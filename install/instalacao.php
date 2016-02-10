e2400a148e6bcd87e2dba14fec86f5dbe85795bb
Marcia Silva <contato.pix@gmail.com>
2016-02-10 21:34:41 -0200

    Ajustes e simplificação do script

--- install/instalacao.php
+++ install/instalacao.php
@@ -1,18 +1,56 @@
 <?php
-require_once '../src/config.php';
-$config_file = APP . 'config.php';
+session_start();
+$config_file = '../src/config.php';
+require_once $config_file;
+require_once APP . 'funcoes.php'; 
+$sql = file_get_contents(INSTALL . 'pages.sql');
+
+$host = trim($_POST['host']);
+$nomeBD = trim($_POST['nome-bd']);
+$login = trim($_POST['login']);
+$senha = trim($_POST['senha']);
 
 if(array_search('', $_POST)){
     echo 'Não adianta desabilitar o Javascript ... tem que preencher todos os campos do formulário!';
     return false;
 } else {
+    
+    $_SESSION['host'] = $host;
+    $_SESSION['nomeBD'] = $nomeBD;
+    $_SESSION['login'] = $login;
+    $_SESSION['senha'] = $senha;
+    
     $conteudo = "\n\n" . 
         '//Banco de dados' . "\n" .
-        '$bd[\'host\'] = \''.  $_POST['host'] . '\';' . "\n" .
-        '$bd[\'nome_bd\'] = \''.  $_POST['nome-bd'] . '\';' . "\n" .
+        '$bd[\'host\'] = \''.  $host . '\';' . "\n" .
+        '$bd[\'nome_bd\'] = \''.  $nomeBD . '\';' . "\n" .
         '$bd[\'tabela\'] = \'pages\';' . "\n" .
-        '$bd[\'login\'] = \''.  $_POST['login'] . '\';' . "\n" .
-        '$bd[\'senha\'] = \''.  $_POST['senha'] . '\';' . "\n";
+        '$bd[\'login\'] = \''.  $login . '\';' . "\n" .
+        '$bd[\'senha\'] = \''.  $senha . '\';' . "\n"; 
     
-    echo @file_put_contents($config_file, $conteudo, FILE_APPEND) ? 'ok' : 'erro';    
+    $cnx = conectaBd($_SESSION['host'], $_SESSION['nomeBD'], $_SESSION['login'], $_SESSION['senha']);
+    
+    if($cnx && file_put_contents($config_file, $conteudo, FILE_APPEND)){
+        echo 'ok';
+        $cnx->query("DROP TABLE IF EXISTS pages;");
+        $cnx->query(
+            'CREATE TABLE `pages` (
+                        `id_pages` int(10) unsigned NOT NULL,
+                        `title_pages` text NOT NULL,
+                        `slug_page` text NOT NULL,
+                        `excerpt_pages` text,
+                        `date_pages` datetime NOT NULL,
+                        `author_pages` varchar(100) NOT NULL,
+                        `content_pages` longtext
+                   ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;'
+        );       
+            
+    }
+    elseif($cnx->query($sql)){
+        echo 'fim';
+    } else {
+        echo 'erro';
+        session_destroy();      
+    } //else
 }
+
