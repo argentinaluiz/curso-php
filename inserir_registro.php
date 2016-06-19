0ec215209ad4ac7bacb39335982efeadf442fb1a
Marcia Silva <contato.pix@gmail.com>
2016-06-19 17:50:41 -0300

    Inserido botão cancelar e link para voltar para Home

--- inserir_registro.php
+++ inserir_registro.php
@@ -18,7 +18,12 @@
     </style>
     <body>
         <div class="container">
-            <h1 class="text-center"><span class="glyphicon glyphicon-plus-sign"></span> Registro de novo aluno</h1>            
+            <h1 class="text-center"><span class="glyphicon glyphicon-plus-sign"></span> Registro de novo aluno</h1> 
+            <p class="text-center">
+                <a href="index.php" class="btn btn-primary" style="display: inline-block; margin-bottom: 20px">
+                    <span class="glyphicon glyphicon-home"></span> Página inicial
+                </a>
+            </p>
             <hr>
             <div class="row">
                 <div class="col-sm-6 col-sm-offset-3">
@@ -32,7 +37,8 @@
                             <input type="number" name="nota" id="nota" class="form-control" min="0" max="10">                            
                         </div>                        
                         <div class="col-sm-12">
-                            <input type="submit" value="Registrar" class="btn btn-primary" style="margin-top: 10px">
+                            <input type="submit" value="Registrar" class="btn btn-success" style="margin-top: 10px">
+                            <a href="index.php" class="btn btn-danger" style="margin: 10px 0 0 10px">Cancelar</a>
                         </div>                        
                     </form>
                     <div class="col-sm-12" id="status_inserir" style="margin-top: 20px"></div>
