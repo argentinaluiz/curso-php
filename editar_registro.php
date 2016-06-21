b99ff90a2d8d25c1e043313bec7f3854b1f49d93
Marcia Silva <contato.pix@gmail.com>
2016-06-20 21:16:33 -0300

    Arquivos para editar registro

--- editar_registro.php
+++ editar_registro.php
@@ -1,19 +1,24 @@
 <?php 
 
-include_once 'src/Database.php';
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
-$bd_host = 'localhost';
-$bd_nome = 'notas';
-$bd_login = 'root';
-$bd_senha = '';
+if(empty($id) || !isset($id)){
+    header('location: index.php');
+} else {
+    include_once 'src/Database.php';
 
-try {
-    $conexao = new PDO("mysql:host=$bd_host;dbname=$bd_nome; charset=utf8", $bd_login, $bd_senha, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
-} catch(PDOException $e) {
-    die('Erro: ' . $e->getCode() . ' - ' . $e->getMessage());
-}
-$alunos = new Database($conexao);
+    $bd_host = 'localhost';
+    $bd_nome = 'notas';
+    $bd_login = 'root';
+    $bd_senha = '';
 
+    try {
+        $conexao = new PDO("mysql:host=$bd_host;dbname=$bd_nome; charset=utf8", $bd_login, $bd_senha, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
+    } catch(PDOException $e) {
+        die('Erro: ' . $e->getCode() . ' - ' . $e->getMessage());
+    }
+    $listar = new Database($conexao);
+    $aluno = $listar->listar_pelo_id($id);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -35,7 +40,7 @@ $alunos = new Database($conexao);
     </style>
     <body>
         <div class="container">
-            <h1 class="text-center"><span class="glyphicon glyphicon-plus-sign"></span> Registro de novo aluno</h1> 
+            <h1 class="text-center"><span class="glyphicon glyphicon-pencil"></span> Editar registro de aluno</h1> 
             <p class="text-center">
                 <a href="index.php" class="btn btn-primary" style="display: inline-block; margin-bottom: 20px">
                     <span class="glyphicon glyphicon-home"></span> Página inicial
@@ -44,21 +49,26 @@ $alunos = new Database($conexao);
             <hr>
             <div class="row">
                 <div class="col-sm-6 col-sm-offset-3">
-                    <form id="inserir_aluno" class="form-group" method="post">
+                    <form id="editar_aluno" class="form-group" method="post">
+                        <div class="col-sm-3">
+                            <label for="id">ID do aluno</label>
+                            <input type="text" name="id" id="id" class="form-control" value="<?php echo $aluno['aluno_id'] ?>" readonly="true">
+                        </div>
                         <div class="col-sm-12">
                             <label for="nome">Nome do aluno</label>
-                            <input type="text" name="nome" id="nome" class="form-control">
+                            <input type="text" name="nome" id="nome" class="form-control" value="<?php echo $aluno['aluno_nome'] ?>">
                         </div>
                         <div class="col-sm-5">
                             <label for="nota">Nota<small> (Insira nota de 0 a 10)</small></label>
-                            <input type="number" name="nota" id="nota" class="form-control" min="0" max="10">                            
+                            <input type="number" name="nota" id="nota" class="form-control" min="0" max="10" value="<?php echo $aluno['aluno_nota'] ?>">                            
                         </div>                        
                         <div class="col-sm-12">
-                            <input type="submit" value="Registrar" class="btn btn-success" style="margin-top: 10px">
+                            <input type="submit" value="Alterar" class="btn btn-success" style="margin-top: 10px">
                             <a href="index.php" class="btn btn-danger" style="margin: 10px 0 0 10px">Cancelar</a>
+                            <input type="hidden" name="id" value="<?php echo $aluno['aluno_id'] ?>">
                         </div>                        
                     </form>
-                    <div class="col-sm-12" id="status_inserir" style="margin-top: 20px"></div>
+                    <div class="col-sm-12" id="status" style="margin-top: 20px"></div>
                 </div>
             </div>         
             
@@ -69,3 +79,5 @@ $alunos = new Database($conexao);
     <script src="js/custom.js"></script>
   </body>
 </html>
+
+<?php } ?>
