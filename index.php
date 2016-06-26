44d343f98f8bce3bc9d42d58a33029080733c93b
Marcia Silva <contato.pix@gmail.com>
2016-06-26 13:46:37 -0300

    Página de login

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    require 'src/config.php';
+    include_once LAYOUT . 'code_head.php'; 
+?>
+    <body>
+        <h1>Hello World!</h1>
+        
+    <?php include_once LAYOUT . 'code_footer.php'; ?>    
+  </body>
+</html>
