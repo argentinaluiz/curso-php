270951b52b8dc7f0755d61cc3e6fa3dabdd4a0d7
Marcia Silva <contato.pix@gmail.com>
2016-06-26 13:46:15 -0300

    Inserirdas constantes de sistema

--- src/config.php
+++ src/config.php
@@ -1,7 +1,12 @@
 <?php
 
-//Bando de dados
+//BANCO DE DADOS =========================
 $bd_host = 'localhost';
 $bd_nome = 'notas';
 $bd_login = 'root';
 $bd_senha = '';
+
+//PASTAS DE SISTEMA =========================
+define('ROOT', '/');
+define('LAYOUT', '/blocks/');
+define('JS', '/js/');
