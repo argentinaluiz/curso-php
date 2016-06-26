244a37b938b0394ce27ebf0df8027d4e26bfcc17
Marcia Silva <contato.pix@gmail.com>
2016-06-26 14:01:03 -0300

    Formulário de login

--- index.php
+++ index.php
@@ -3,7 +3,42 @@
     include_once LAYOUT . 'code_head.php'; 
 ?>
     <body>
-        <h1>Hello World!</h1>
+        <!-- ####### Formulario de login ####### -->
+        <div class="container">
+            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+
+                <div class="panel panel-default" style="margin-top: 30px">
+                    <!-- Titulo -->
+                    <div class="panel-heading">
+                        <div class="panel-title">
+                            <h2 class="text-center">Login</h2>
+                        </div>
+                    </div>
+
+                    <div id="login-form" class="panel-body" >
+                        <form id="login-admin" method="post" action="../src/login_script.php">
+                            <div class="input-group" style="margin-bottom: 8px">
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-user"></span>
+                                </span>
+                                <input type="text" name="login" class="form-control" placeholder="Usuário">
+                            </div>
+                            <div class="input-group" style="margin-bottom: 8px">
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-option-horizontal"></span>
+                                </span>
+                                <input type="password" name="senha" class="form-control" placeholder="Senha">
+                            </div>
+                            <div class="text-center">
+                                <input type="submit" value="Login" class="btn btn-primary">
+                            </div>
+                        </form>                        
+                    </div>
+
+                </div>
+
+            </div>
+        </div><!-- /Formulario de login -->
         
     <?php include_once LAYOUT . 'code_footer.php'; ?>    
   </body>
