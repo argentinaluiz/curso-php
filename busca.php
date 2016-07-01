154f754062bd408d9efeb87f9e9796594efd566d
Marcia Silva <contato.pix@gmail.com>
2016-07-01 20:46:35 -0300

    Inserida variável armazenamento da busca vinda via GET

--- busca.php
+++ busca.php
@@ -5,6 +5,8 @@ include_once '/src/config.php';
 include_once SRC . 'Database.php';
 include_once SRC . 'Login.php';
 
+$buscar_por = filter_input(INPUT_GET, 'busca-aluno', FILTER_SANITIZE_STRING);
+
 //Tenta conectar
 try {
     $conexao = new PDO("mysql:host=$bd_host;dbname=$bd_nome; charset=utf8", $bd_login, $bd_senha, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
@@ -15,17 +17,14 @@ try {
 $aut = new Login($conexao);
 
 //Se o usuario esta logado
-if($aut->session_check()){
+if($aut->session_check()){    
 
-    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-
-    if(empty($id) || !isset($id)){
-        header('location: index.php');
+    if(empty($buscar_por) || !isset($buscar_por)){
+        header('location: ' . ROOT);
     } else {
         
         $listar = new Database($conexao);
-        $aluno = $listar->listar_pelo_id($id);
-
+        
         include_once LAYOUT . 'code_head.php';
     ?>
     <style>
@@ -33,7 +32,7 @@ if($aut->session_check()){
     </style>
     <body>
         <div class="container">
-            <h1 class="text-center"><span class="glyphicon glyphicon-pencil"></span> Editar registro de aluno</h1> 
+            <h1 class="text-center"><span class="glyphicon glyphicon-search"></span> Resultado da busca</h1> 
             <p class="text-center">
                 <a href="index.php" class="btn btn-primary" style="display: inline-block; margin-bottom: 20px">
                     <span class="glyphicon glyphicon-home"></span> Página inicial
@@ -43,30 +42,43 @@ if($aut->session_check()){
                 </a>
             </p>
             <hr>
+            
             <div class="row">
-                <div class="col-sm-6 col-sm-offset-3">
-                    <form id="editar_aluno" class="form-group" method="post">
-                        <div class="col-sm-3">
-                            <label for="id">ID do aluno</label>
-                            <input type="text" name="id" id="id" class="form-control" value="<?php echo $aluno['aluno_id'] ?>" readonly="true">
-                        </div>
-                        <div class="col-sm-12">
-                            <label for="nome">Nome do aluno</label>
-                            <input type="text" name="nome" id="nome" class="form-control" value="<?php echo $aluno['aluno_nome'] ?>">
-                        </div>
-                        <div class="col-sm-5">
-                            <label for="nota">Nota<small> (Insira nota de 0 a 10)</small></label>
-                            <input type="number" name="nota" id="nota" class="form-control" min="0" max="10" value="<?php echo $aluno['aluno_nota'] ?>">                            
-                        </div>                        
-                        <div class="col-sm-12">
-                            <input type="submit" value="Alterar" class="btn btn-success" style="margin-top: 10px">
-                            <a href="<?php echo ROOT ?>" class="btn btn-danger" style="margin: 10px 0 0 10px">Cancelar</a>
-                            <input type="hidden" name="id" value="<?php echo $aluno['aluno_id'] ?>">
-                        </div>                        
-                    </form>
-                    <div class="col-sm-12" id="status" style="margin-top: 20px"></div>
-                </div>
-            </div>         
+                    <div class="col-sm-6 col-sm-offset-3">                        
+
+                        <table class="table table-striped">                            
+                            <tr>
+                                <th class="col-sm-1">ID</th>
+                                <th class="col-sm-4">Nome</th>
+                                <th class="col-sm-2">Nota</th>
+                                <th colspan="2" class="col-sm-5"></th>
+                            </tr>
+                            <?php
+
+                                if($listar->listar_busca($buscar_por) > 0){
+                                    foreach($listar->listar_busca($buscar_por) as $todos){?>
+                                    <tr>
+                                        <td><?php echo $todos['aluno_id'] ?></td>
+                                        <td><a href="mostrar_registro.php?id=<?php echo $todos['aluno_id'] ?>"><?php echo $todos['aluno_nome'] ?></a></td>
+                                        <td><?php echo $todos['aluno_nota'] ?></td>
+
+                                        <td>
+                                            <a href="editar_registro.php?id=<?php echo $todos['aluno_id'] ?>" class="label label-primary"><span class="glyphicon glyphicon-pencil"></span> Editar</a>
+                                        </td>
+                                        <td>
+                                            <a href="#" data-id="<?php echo $todos['aluno_id'] ?>" class="label label-danger link-excluir" style="border: none">
+                                                <span class="glyphicon glyphicon-remove"></span> Excluir
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <?php }
+                                } else {
+                                    echo '<tr><td colspan="3">Nenhum registro encontrado</td></tr>';
+                                }
+                            ?>
+                        </table>
+                    </div>
+                </div>         
             
         </div>
     
