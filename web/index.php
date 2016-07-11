5e40a2a5e1765d6a94a9f68855457475df7304b5
Marcia Silva <contato.pix@gmail.com>
2016-07-11 16:22:05 -0300

    Organização do código

--- web/index.php
+++ web/index.php
@@ -2,10 +2,29 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Response;
+
 $app = new Silex\Application();
 $app['debug'] = true;
 
-//Montando as rotas
+//SERVICOS =================================
+
+//BD
+$app['lista_de_posts'] = $app->share(function(){
+    include 'bd.php';
+});
+
+//CONTROLLERS =================================
+//index
+$index = $app['controllers_factory'];
+
+//Index do site
+$index->get('/', function() use($app){
+    return new Response('<h1>Home</h1><p><a href="/posts">Ir para a lista de posts</a></p>');
+});
+
+//ROTAS =================================
+$app->mount('/', $index);
 $app->mount('/posts', include 'posts.php');
 
 //Vai!
