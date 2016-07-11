d051c7aece6568739aae219d9e3432ea6f04c0f9
Marcia Silva <contato.pix@gmail.com>
2016-07-11 16:35:21 -0300

    Reorganização do código

--- web/index.php
+++ web/index.php
@@ -8,22 +8,20 @@ $app = new Silex\Application();
 $app['debug'] = true;
 
 //SERVICOS =================================
-
 //BD
 $app['lista_de_posts'] = $app->share(function(){
     include 'bd.php';
+    return $bd_lista;
 });
 
-//CONTROLLERS =================================
+//CONTROLLERS ===============================
 //index
 $index = $app['controllers_factory'];
-
-//Index do site
 $index->get('/', function() use($app){
     return new Response('<h1>Home</h1><p><a href="/posts">Ir para a lista de posts</a></p>');
 });
 
-//ROTAS =================================
+//ROTAS =====================================
 $app->mount('/', $index);
 $app->mount('/posts', include 'posts.php');
 
