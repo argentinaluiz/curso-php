d6a8dbc8c30bd2a752242802e32ce80bbcb37b98
Marcia Silva <contato.pix@gmail.com>
2016-08-25 20:41:16 -0300

    Saídas HTML transferido para as views gerenciado pelo Twig

--- web/posts.php
+++ web/posts.php
@@ -13,39 +13,13 @@ $posts_lista = $app['lista_de_posts'];
 $posts = $app['controllers_factory'];
 
 //Lista de posts - URL posts Nivel 1
+
 $posts->get('/', function(App $app) use ($posts_lista){
-    if(!empty($posts_lista)){
-        
-        $html = '';
-        $html .= '<h1>Hello World!</h1>';
-        foreach ($posts_lista as $key => $post){            
-            $html .= '<h2><b>ID:</b> <a href="' . $app['url_generator']->generate('lista-posts') . $post['id'] . '">' . $post['id'] . '</a></h2>';
-            $html .= '<p>' . $post['conteudo'] . '</p>';
-            $html .= '<hr>';
-        }
-        
-        return new Response($html);
-    } else {
-        return new Response('Nenhum registro encontrado');
-    }  
+    return $app['twig']->render('posts.twig', array('posts' => $posts_lista));
 })->bind('lista-posts');
 
 //Posts único - URL posts Nivel 2
-$posts->get('/{id}', function(App $app, $id) use ($posts_lista){
-    
-    foreach ($posts_lista as $post){
-        if($post['id'] == $id){
-            $html = '<h2>ID: ' . $post['id'] . '</h2>';
-            $html .= '<p>' . $post['conteudo'] . '</p>';
-            $html .= '<p><a href="' . $app['url_generator']->generate('lista-posts') . '" class="btn btn-default">Voltar para a lista</a></p>';
-        }
-    }
-    
-    if(!empty($html)){
-        return new Response($html);
-    } else {
-        $app->abort(404, 'O post com o ID ' . $id . ' não existe');
-    }
+$posts->get('/{id}', function(App $app, $id) use ($posts_lista){    
 });
 
 //IMPORTANTE! Retornar controller
