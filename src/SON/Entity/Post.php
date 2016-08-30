9a7533f58321fa0e5957342b5a3c1435ada492a4
Marcia Silva <contato.pix@gmail.com>
2016-08-30 19:19:04 -0300

    Criada entidade Post

---
+++ src/SON/Entity/Post.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace SON\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="posts")
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+    
+    /**
+     * @ORM\Column(type="string", lenght=300)
+     */
+    private $titulo;
+    
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $conteudo;
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    public function getConteudo()
+    {
+        return $this->conteudo;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+        return $this;
+    }
+
+    public function setConteudo($conteudo)
+    {
+        $this->conteudo = $conteudo;
+        return $this;
+    }
+    
+}
+
