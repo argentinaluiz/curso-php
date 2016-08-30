ebfbff5c1323ef4ec64986490080d74b271970fe
Marcia Silva <contato.pix@gmail.com>
2016-08-30 19:17:59 -0300

    Configurações foram movidas para um arquivo bootstrap

--- web/index.php
+++ web/index.php
@@ -1,26 +1,10 @@
 <?php
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Response;
 
-$app = new Silex\Application();
-$app['debug'] = true;
-
-//PROVIDERS =================================
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__ . '/../views',
-));
-
-//SERVICOS =================================
-//BD
-$app['lista_de_posts'] = $app->share(function(){
-    include 'bd.php';
-    return $bd_lista;
-});
+require_once 'bootstrap.php';
 
 //ROTAS =====================================
 $app->mount('/posts', include 'posts.php');
 $app->run();
-?>
