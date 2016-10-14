cd7c58980dd30d8fe18562e76248740079c04b37
Marcia Silva <contato.pix@gmail.com>
2016-10-14 16:06:13 -0300

    Criada rota inicial

--- web/index.php
+++ web/index.php
@@ -6,5 +6,8 @@ use Symfony\Component\HttpFoundation\Response;
 require_once 'bootstrap.php';
 
 //ROTAS =====================================
+$app->get('/', function(Silex\Application $app){
+    return $app->redirect($app['url_generator']->generate('lista-posts'));
+});
 $app->mount('/posts', include 'posts.php');
 $app->run();
